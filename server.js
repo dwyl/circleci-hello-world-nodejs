db1a1097abfcf95b4ee03f4a222b0c8e3c440d67
nelsonic <contact.nelsonic+github@gmail.com>
2018-06-20 19:13:23 +0100

    adds a few in-line (clarifying) comments to server.js (not that anyone cares ... but I like to make things easy to follow...) :wink:

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 /** a super-basic static page web server with no dependencies **/
-const http = require('http');
-const fs = require('fs');
+const http = require('http'); // basic http server (don't need anything fancy!)
+const fs = require('fs');     // to read index.html and favicon.ico files
 // read files synchronously once when app starts (no "perf" impact):
 const index = fs.readFileSync(__dirname + '/index.html', 'utf8');
 const favicon = fs.readFileSync(__dirname + '/favicon.ico');
 // display the Git revision hash in browser so we know which version of app.
 const get_hash = require('./git_hash.js');
-
-const hello = require('./hello.js');
+const hello = require('./hello.js'); // the most basic "function" ever!
 
 get_hash(function(e, GIT_COMMIT_HASH, stder) {
   !e ? console.log('GIT_COMMIT_HASH:', GIT_COMMIT_HASH) : console.log(e);
